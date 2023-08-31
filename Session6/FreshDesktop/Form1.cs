7900ed389f2f3f8863ed21dc8b9e82dea098b744
Stephen-Sim <simshansiong2002@gmail.com>
2023-08-31 22:13:12 +0800

    session 6: add filtering

--- Session6/FreshDesktop/Form1.cs
+++ Session6/FreshDesktop/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -17,6 +18,40 @@ namespace FreshDesktop
             InitializeComponent();
 
             ent = new WSC2022SE_Session6Entities();
+        }
+
+        void clearFilter()
+        {
+            dateTimePicker1.Value = new DateTime(2022, 01, 01);
+            dateTimePicker2.Value = new DateTime(2022, 01, 01);
+
+            var areas = ent.Areas.ToList().OrderBy(a => a.Name).ToList();
+            comboBox1.DataSource = areas;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "ID";
+            comboBox1.SelectedIndex = -1;
+
+            var hosts = ent.Users.ToList().Where(x => x.Items.Count > 0).OrderBy(x => x.FullName).ToList();
+            comboBox2.DataSource = hosts;
+            comboBox2.DisplayMember = "FullName";
+            comboBox2.ValueMember = "ID";
+            comboBox2.SelectedIndex = -1;
+
+            var guests = ent.Users.ToList().Where(x => x.Bookings.Count > 0).OrderByDescending(x => x.Bookings.Count).ToList();
+            comboBox3.DataSource = guests;
+            comboBox3.DisplayMember = "FullName";
+            comboBox3.ValueMember = "ID";
+            comboBox3.SelectedIndex = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clearFilter();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearFilter();
         }
     }
 }
